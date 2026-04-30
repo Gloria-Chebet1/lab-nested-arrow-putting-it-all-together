b12e4030372f5dd9e4270b5e4c3624f9c09706e5
Gloria Chebet <gloriachebetkerubo@gmail.com>
2026-04-30 19:05:06 +0300

    password

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+function createLoginTracker(userInfo) {        
+  let attemptCount = 0;
+  const MAX_ATTEMPTS = 3;
+
+  const loginAttempt = (passwordAttempt) => {   
+    if (attemptCount >= MAX_ATTEMPTS) {          
+      return "Account locked. Too many attempts";
+    }
+    if (passwordAttempt === userInfo.password) {
+      attemptCount = 0;
+      return "Login successful";
+    }
+    attemptCount++;
+    const left = MAX_ATTEMPTS - attemptCount;
+    return `Attempt ${attemptCount} failed. ${left} left`;
+  };
+
+  return loginAttempt;                          
+}
 
 
 
