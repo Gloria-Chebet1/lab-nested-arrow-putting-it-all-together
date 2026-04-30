5c8dfdcad86e3412c0e2bc577bd0e76741aeba22
Gloria Chebet <gloriachebetkerubo@gmail.com>
2026-04-30 19:18:43 +0300

    password

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 function createLoginTracker(userInfo) {
-    
     let wrongLoginCount = 0;
 
-    
     const loginAttempt = (passwordAttempt) => {
-        
         if (wrongLoginCount >= 3) {
             return "Account locked due to too many failed login attempts";
         }
 
-        // Correct password: allow login immediately
         if (passwordAttempt === userInfo.password) {
             return "Login successful";
         }
 
-        
         wrongLoginCount++;
 
-        
-        if (wrongLoginCount <= 3) {
-            return `Login attempt ${wrongLoginCount}: Login failed`;
+        if (wrongLoginCount === 3) {
+            return "Account locked due to too many failed login attempts";
         }
 
-      
-        return "Account locked due to too many failed login attempts";
+        return `Login attempt ${wrongLoginCount}: Login failed`;
     };
 
     return loginAttempt;
